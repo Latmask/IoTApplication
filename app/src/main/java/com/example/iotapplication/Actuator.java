19665c80789be20b4f04acb8c6b3fff1d0692f4b
Latmask <105066980+Latmask@users.noreply.github.com>
2022-12-05 19:52:53 +0100

    Voice reasoner is functional
    The voice reasoner is now functional, its possible to manipulate the status of the light objects through voice commands. If you try to turn on, turn off or check the status of a light which isn't in the light list the app will tell that there is no such light

--- app/src/main/java/com/example/iotapplication/Actuator.java
+++ app/src/main/java/com/example/iotapplication/Actuator.java
@@ -2,16 +2,20 @@ package com.example.iotapplication;
 
 public abstract class Actuator {
     private String name;
+    private String numName;
     private Boolean status;
 
-    public Actuator(String name, Boolean status){
+    public Actuator(String name, String numName, Boolean status){
         this.name = name;
+        this.numName = numName;
         this.status = status;
     }
 
     public String getName(){
         return name;
     }
+
+    public String getNumName(){ return numName;}
 
     public Boolean getStatus(){
         return status;
@@ -26,7 +30,7 @@ public abstract class Actuator {
     }
 
     public String toString() {
-        return name + ";" + status;
+        return name + ";" + numName + ";" + status;
     }
 
 }
