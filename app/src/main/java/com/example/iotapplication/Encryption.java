00696950af71ee2aba7414bb1c2dc09eb5d16806
FerminCho <fermin.liljeqvist@gmail.com>
2022-12-09 13:59:09 +0100

    Fixed settings

--- app/src/main/java/com/example/iotapplication/Encryption.java
+++ app/src/main/java/com/example/iotapplication/Encryption.java
@@ -1,5 +1,44 @@
 package com.example.iotapplication;
 
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.util.Date;
+import java.util.Calendar;
+
 public class Encryption {
 
+
+    public void InstanceGenerator() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
+        Date startDate = new Date();
+
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.YEAR, 1);
+        Date endDate = c.getTime();
+
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_RSA, "AndroidKeyStore");
+
+        keyPairGenerator.initialize(
+                new KeyGenParameterSpec.Builder(
+                        "key1",
+                        KeyProperties.PURPOSE_SIGN)
+                        .setDigests(KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
+                        .setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PSS)
+                        .setCertificateNotBefore(startDate)
+                        .setCertificateNotAfter(endDate)
+                        .build());
+
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+    }
+
+
+
+
+
 }
+
