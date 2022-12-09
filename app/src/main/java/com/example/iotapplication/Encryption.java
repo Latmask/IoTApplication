80b76c669a6cc066118d819378adf2babe47af68
FerminCho <fermin.liljeqvist@gmail.com>
2022-12-09 17:05:45 +0100

    Added code for creating asymetrtical keys

--- app/src/main/java/com/example/iotapplication/Encryption.java
+++ app/src/main/java/com/example/iotapplication/Encryption.java
@@ -4,17 +4,19 @@ import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
 
 import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.Signature;
 import java.util.Date;
 import java.util.Calendar;
 
 public class Encryption {
 
 
-    public void InstanceGenerator() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
+    public void InstanceGenerator() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException, InvalidKeyException {
         Date startDate = new Date();
 
         Calendar c = Calendar.getInstance();
@@ -29,11 +31,13 @@ public class Encryption {
                         KeyProperties.PURPOSE_SIGN)
                         .setDigests(KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
                         .setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PSS)
-                        .setCertificateNotBefore(startDate)
-                        .setCertificateNotAfter(endDate)
+                        .setKeyValidityStart(startDate)
+                        .setKeyValidityEnd(endDate)
                         .build());
 
         KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        Signature signature = Signature.getInstance("SHA256withRSA/PSS");
+        signature.initSign(keyPair.getPrivate());
     }
 
 
