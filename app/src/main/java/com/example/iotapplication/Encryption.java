986b92ca1339efedb636ddf0907ff4f5a7862cea
FerminCho <fermin.liljeqvist@gmail.com>
2022-12-12 18:05:10 +0100

    Added AES encryption method

--- app/src/main/java/com/example/iotapplication/Encryption.java
+++ app/src/main/java/com/example/iotapplication/Encryption.java
@@ -20,6 +20,9 @@ import java.security.cert.CertificateException;
 import java.util.Date;
 import java.util.Calendar;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
 public class Encryption {
 
 
@@ -34,24 +37,44 @@ public class Encryption {
 
         keyPairGenerator.initialize(
                 new KeyGenParameterSpec.Builder(
-                        "key1",
+                        "asyncKey",
                         KeyProperties.PURPOSE_SIGN)
                         .setDigests(KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
                         .setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PSS)
                         .setKeyValidityStart(startDate)
                         .setKeyValidityEnd(endDate)
+                        /*.setIsStrongBoxBacked(true)*/
                         .build());
 
         KeyPair keyPair = keyPairGenerator.generateKeyPair();
         Signature signature = Signature.getInstance("SHA256withRSA/PSS");
 
         KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
-        PrivateKey privateKey = (PrivateKey) keyStore.getKey("key1", null);
-        PublicKey publicKey = keyStore.getCertificate("key1").getPublicKey();
+        keyStore.load(null);
+        PrivateKey privateKey = (PrivateKey) keyStore.getKey("asyncKey", null);
+        PublicKey publicKey = keyStore.getCertificate("asyncKey").getPublicKey();
         signature.initSign(privateKey);
 
         String object = "example";
         byte[] signedObject = signature.update(object.toByteArray()).sing();
+    }
+
+    public void AESEncryption() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException, KeyStoreException, UnrecoverableKeyException, CertificateException, IOException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+
+        keyGenerator.init(
+                new KeyGenParameterSpec.Builder("syncKey",
+                        KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                        .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                        .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                        .build());
+        SecretKey key = keyGenerator.generateKey();
+        KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
+        keyStore.load(null);
+        key = (SecretKey) keyStore.getKey("key2", null);
+
+
+
     }
 
 
